f86d2dd157eb714ae36b75345e7332b8abff6878
Mouhamed Diagne <diagnemouhamedrassoul87@gmail.com>
2026-05-03 23:58:49 +0200

    Initial commit SenTransport

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur SenTransport</h1>
+      <p>Le transport en commun à Dakar</p>
+    </div>
+  );
+}
+
+export default App;
